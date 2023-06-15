1c6e21aa83c1f5b9c52ccdf5c1a9e635cff8aedf
salmaasal <salameri482@gmail.com>
2023-06-15 12:14:32 +0400

    Initial Commit

---
+++ cardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cardApp
+//
+//  Created by MAC008634 on 15/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

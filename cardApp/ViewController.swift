1dc2dbbf91a863ee2663e4df76f83adb400c1c0b
salmaasal <salameri482@gmail.com>
2023-06-19 09:52:10 +0400

    first commit

--- cardApp/ViewController.swift
+++ cardApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func cardPressed(_ sender: UIButton) {
+        flipCard(withEmoji: "😈", on: sender)
     }
 
+    @IBAction func cardPressedTwo(_ sender: UIButton) {
+        flipCard(withEmoji: "🫀", on: sender)
+    }
+    func flipCard(withEmoji emoji: String, on button:UIButton) {
+        print("flipCard(withEmoji: \(emoji))")
+        if button.currentTitle == emoji {
+            button.setTitle ("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        } else {
+            button.setTitle (emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 
 }
 
